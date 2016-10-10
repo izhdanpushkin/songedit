db6ff4f0165a94810d5312a4792b75c9f5fc6bfc
Azuny <crippyua@gmail.com>
2016-10-10 23:47:46 +0300

    Make meta-to-name.py PEP8 compliant

--- meta-to-name.py
+++ meta-to-name.py
@@ -1,26 +1,29 @@
 # Rename songs from their metadata
-import os, eyed3
-myPath       = './'
-songlist     = os.listdir(myPath)
-songlistCopy = songlist[:]
-songlistNew  = []
+import os
+import eyed3
+
+myPath = './'
+songlist = os.listdir(myPath)
+songlistNew = []
 
 for song in range(0, len(songlist)):
     songlistNew.append('')
-    counter    = 0
-    spacing    = ' '
+    counter = 0
+    spacing = ' '
     songArtist = ''
-    songTitle  = ''
-    if songlist[song].find('.mp3') == -1: # only change .mp3 files
+    songTitle = ''
+    if songlist[song].find('.mp3') == -1:  # only change .mp3 files
         continue
 
     file = eyed3.load(songlist[song])
 
     songArtist = file.tag.artist
-    songTitle  = file.tag.title
+    songTitle = file.tag.title
 
-    if not file.tag is None and not file.tag.artist is None and not file.tag.title is None:
-        songName          = str(songArtist.encode('utf-8') + ' - ' + songTitle.encode('utf-8') + '.mp3')
+    if file.tag is not None and file.tag.artist is not None and
+    file.tag.title is not None:
+        songName = str(songArtist.encode('utf-8') + ' - ' +
+                       songTitle.encode('utf-8') + '.mp3')
         songlistNew[song] = songName
     else:
         songName = songlist[song]
@@ -31,6 +34,7 @@ for song in range(0, len(songlist)):
             songName = songName.replace('/', ' ')
 
     if songlist[song] != songlistNew[song]:
-        print('Was: ' + songlist[song] + '\n' + 'Now: ' + songlistNew[song].strip() + '\n')
+        print('Was: ' + songlist[song] + '\n' + 'Now: ' +
+              songlistNew[song].strip() + '\n')
     # print songlist[song]
     os.rename(myPath + songlist[song], myPath + songName)
