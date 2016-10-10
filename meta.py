c3fa93d344baa7fd1f9d391780d38e386f561f67
Azuny <crippyua@gmail.com>
2016-10-10 23:40:28 +0300

    Make meta.py PEP8 compliant

--- meta.py
+++ meta.py
@@ -1,21 +1,23 @@
 # Create metadata from song file name
-import os, eyed3
-myPath       = './'
-songlist     = os.listdir(myPath)
-songlistNew  = []
+import os
+import eyed3
+
+myPath = './'
+songlist = os.listdir(myPath)
+songlistNew = []
 
 for song in range(0, len(songlist)):
     songlistNew.append('')
-    counter    = 0
-    spacing    = ' '
+    counter = 0
+    spacing = ' '
     songArtist = ''
-    songTitle  = ''
-    if songlist[song].find('.mp3') == -1: # only change .mp3 files
+    songTitle = ''
+    if songlist[song].find('.mp3') == -1:  # only change .mp3 files
         continue
 
     if songlist[song].find(' - ') != -1:
         songArtist = songlist[song].split(' - ')[0].decode('utf-8', 'replace')
-        songTitle  = songlist[song].split(' - ')[1].decode('utf-8', 'replace')
+        songTitle = songlist[song].split(' - ')[1].decode('utf-8', 'replace')
     else:
         continue
 
@@ -27,11 +29,11 @@ for song in range(0, len(songlist)):
     # tags are weird, this seem to work
     try:
         print songlist[song], songArtist
-        if eyed3.load(songlist[song]).tag.title == None:
+        if eyed3.load(songlist[song]).tag.title is None:
             # changing metadata of the song if none present
             file.tag.title = songTitle[0:(len(songTitle) - 4)]
             file.tag.save()
-        if eyed3.load(songlist[song]).tag.artist == None:
+        if eyed3.load(songlist[song]).tag.artist is None:
             file.tag.artist = songArtist
             file.tag.save()
     except NotImplementedError:
@@ -40,13 +42,14 @@ for song in range(0, len(songlist)):
         file.tag.artist = u'%s' % songArtist
         file.tag.save()
 
-    if eyed3.load(songlist[song]).tag.title == None:
+    if eyed3.load(songlist[song]).tag.title is None:
         # changing metadata of the song if none present
         file.tag.title = u'%s' % songTitle[0:(len(songTitle) - 4)]
         file.tag.save()
-    if eyed3.load(songlist[song]).tag.artist == None:
+    if eyed3.load(songlist[song]).tag.artist is None:
         file.tag.artist = u'%s' % songArtist
         file.tag.save()
 
-    # if songlist[song] != songlistNew[song]:
-    #     print('Was: ' + songlist[song] + '\n' + 'Now: ' + songlistNew[song].strip() + '\n')
+    # if songlist[song] != songlistNew[song]:     print('Was: ' +
+    # songlist[song] + '\n' + 'Now: ' + songlistNew[       4 song].strip() +
+    # '\n')
