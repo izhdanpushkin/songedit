af6da73bcf42c5167ddb370165e33fcd8a5ae05e
Azuny <crippyua@gmail.com>
2016-10-17 15:38:31 +0300

    Implement meta_to_name function

--- songedit.py
+++ songedit.py
@@ -5,14 +5,12 @@ import eyed3
 
 myPath = './'
 songlist = os.listdir(myPath)
-songlistNew = []
 
 def meta():
-    '''Create metadata for artist and song name if none present.'''
+    '''Create metadata for artist and song name if none present for all mp3
+     files in current working directory.
+    '''
     for song in range(0, len(songlist)):
-        songlistNew.append('')
-        songArtist = ''
-        songTitle = ''
         if songlist[song].find('.mp3') == -1:  # only change .mp3 files
             continue
 
@@ -32,9 +30,10 @@ def meta():
 
         # tags are weird, this seems to work
         try:
-            # changing metadata of the song if none present
+            # change metadata of the song if none present
             if eyed3.load(songlist[song]).tag.title is None:
-                file.tag.title = songTitle[0:(len(songTitle) - 4)]
+                # file.tag.title = songTitle[0:(len(songTitle) - 4)]
+                file.tag.title = songTitle[0:-4]
                 file.tag.save()
             if eyed3.load(songlist[song]).tag.artist is None:
                 file.tag.artist = songArtist
@@ -42,9 +41,46 @@ def meta():
             print songlist[song]
         except NotImplementedError:
             file.initTag()
-            file.tag.title = u'%s' % songTitle[0:(len(songTitle) - 4)]
+            # file.tag.title = u'%s' % songTitle[0:(len(songTitle) - 4)]
+            file.tag.title = u'%s' % songTitle[0:-4]
             file.tag.artist = u'%s' % songArtist
             file.tag.save()
             print songlist[song]
         return
-meta()
+
+def meta_to_name():
+    '''Change filenames of all mp3 files in current working directory according
+    to their metadata'''
+    songlistNew = []
+    for song in range(0, len(songlist)):
+        songlistNew.append('')
+        if songlist[song].find('.mp3') == -1:  # only change .mp3 files
+            continue
+        file = eyed3.load(songlist[song])
+
+        # check if tag is present
+        try:
+            songArtist = file.tag.artist
+            songTitle = file.tag.title
+        except:
+            continue
+
+        # try to create a new filename
+        if file.tag.artist is not None and file.tag.title is not None:
+            songName = str(songArtist.encode('utf-8') + ' - ' +
+                           songTitle.encode('utf-8') + '.mp3')
+            songlistNew[song] = songName
+        else:
+            continue
+
+        if '/' in songlistNew[song]:
+            songlistNew[song] = songlistNew[song].replace('/', ' ')
+
+        # make changes and print them out
+        if songlist[song] != songlistNew[song]:
+            print('Was: ' + songlist[song] + '\n' + 'Now: ' +
+                  songlistNew[song].strip() + '\n')
+            os.rename(myPath + songlist[song], myPath + songlistNew[song])
+        return
+# meta()
+# meta_to_name()
